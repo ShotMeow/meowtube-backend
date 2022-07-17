120834e413c1f1514704ecfcbf1ec50be6a1b0cd
ShotMeow <shotmeow@gmail.com>
2022-07-17 11:28:54 +0300

    Configure Auth Service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { JwtStrategy } from './strategies/jwt.strategy'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { getJwtConfig } from '../config/jwt.config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserEntity } from '../user/user.entity'
 
 @Module({
 	controllers: [AuthController],
-	providers: [AuthService, JwtStrategy]
+	providers: [AuthService, JwtStrategy],
+	imports: [
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		}),
+		TypeOrmModule.forFeature([UserEntity])
+	]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+	UnauthorizedException
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserEntity } from '../user/user.entity'
+import { Repository } from 'typeorm'
+import { JwtService } from '@nestjs/jwt'
+import { AuthDto } from './dto/auth.dto'
+import { compare, genSalt } from 'bcryptjs'
+import { hash } from 'typeorm/util/StringUtils'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(
+		@InjectRepository(UserEntity)
+		private readonly userRepository: Repository<UserEntity>,
+		private readonly jwtService: JwtService
+	) {}
+
+	async register(dto: AuthDto) {
+		const oldUser = await this.userRepository.findOneBy({ email: dto.email })
+		if (oldUser) throw new BadRequestException('Email занят')
+
+		const salt = await genSalt(10)
+
+		const newUser = await this.userRepository.create({
+			email: dto.email,
+			password: hash(dto.password, salt)
+		})
+		const user = await this.userRepository.save(newUser)
+
+		return {
+			user: this.returnUserFields(user),
+			accessToken: await this.issueAccessToken(user.id)
+		}
+	}
+
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto)
+
+		return {
+			user: this.returnUserFields(user),
+			accessToken: await this.issueAccessToken(user.id)
+		}
+	}
+
+	async validateUser(dto: AuthDto) {
+		const user = await this.userRepository.findOne({
+			where: {
+				email: dto.email
+			},
+			select: ['id', 'email', 'password']
+		})
+
+		if (!user) throw new NotFoundException('Пользователь не найден.')
+
+		const isValidPassword = await compare(dto.password, user.password)
+		if (!isValidPassword)
+			throw new UnauthorizedException('Не правильный пароль.')
+		return user
+	}
+
+	async issueAccessToken(userId: number) {
+		const data = {
+			id: userId
+		}
+
+		return await this.jwtService.signAsync(data, {
+			expiresIn: '31d'
+		})
+	}
+
+	returnUserFields(user: UserEntity) {
+		return {
+			id: user.id,
+			email: user.email
+		}
+	}
+}
