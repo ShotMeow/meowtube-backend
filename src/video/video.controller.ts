d5bfbb633b6c353c1b89bfb1790f86cf28768d21
ShotMeow <shotmeow@gmail.com>
2022-07-17 20:04:30 +0300

    Finish Video Controller

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,7 +1,75 @@
-import { Controller } from '@nestjs/common';
-import { VideoService } from './video.service';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	Query,
+	Delete
+} from '@nestjs/common'
+import { VideoService } from './video.service'
+import { Auth } from '../auth/decorators/auth.decorator'
+import { CurrentUser } from '../user/decorators/user.decorator'
+import { VideoDto } from './dto/video.dto'
 
 @Controller('video')
 export class VideoController {
-  constructor(private readonly videoService: VideoService) {}
+	constructor(private readonly videoService: VideoService) {}
+
+	@Get('get-private/:id')
+	@Auth()
+	async getVideoPrivate(@CurrentUser('id') id: string) {
+		return this.videoService.getVideoById(+id, true)
+	}
+
+	@Get()
+	async getAllVideos(@Query('searchTerm') searchTerm?: string) {
+		return this.videoService.getAllVideos(searchTerm)
+	}
+
+	@Get('most-popular')
+	async getMostPopularByViews() {
+		return this.videoService.getMostPopularByViews()
+	}
+
+	@Get(':id')
+	async getVideo(@Param('id') id: string) {
+		return this.videoService.getVideoById(+id)
+	}
+
+	@HttpCode(200)
+	@Post()
+	@Auth()
+	async createVideo(@CurrentUser('id') id: number) {
+		return this.videoService.createVideo(id)
+	}
+
+	@HttpCode(200)
+	@Put(':id')
+	@Auth()
+	async updateVideo(@Param('id') id: string, @Body() dto: VideoDto) {
+		return this.videoService.update(+id, dto)
+	}
+
+	@HttpCode(200)
+	@Delete(':id')
+	@Auth()
+	async deleteVideo(@Param('id') id: string) {
+		return this.videoService.deleteVideo(+id)
+	}
+
+	@HttpCode(200)
+	@Put('update-views/:videoId')
+	async updateViews(@Param('videoId') videoId: string) {
+		return this.videoService.updateCountViews(+videoId)
+	}
+
+	@HttpCode(200)
+	@Put('update-likes/:videoId')
+	@Auth()
+	async updateLikes(@Param('videoId') videoId: string) {
+		return this.videoService.updateReaction(+videoId)
+	}
 }
--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -1,4 +1,152 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { VideoEntity } from './video.entity'
+import { ILike, MoreThan, Repository } from 'typeorm'
+import { VideoDto } from './dto/video.dto'
+import { FindOptionsWhereProperty } from 'typeorm'
 
 @Injectable()
-export class VideoService {}
+export class VideoService {
+	constructor(
+		@InjectRepository(VideoEntity)
+		private readonly videoRepository: Repository<VideoEntity>
+	) {}
+
+	async getVideoById(id: number, isPublic = false) {
+		const video = await this.videoRepository.findOne({
+			where: isPublic
+				? {
+						id,
+						isPublic: true
+				  }
+				: {
+						id
+				  },
+			relations: {
+				user: true,
+				comments: {
+					user: true
+				}
+			},
+			select: {
+				user: {
+					id: true,
+					name: true,
+					avatarPath: true,
+					isVerified: true,
+					subscribersCount: true,
+					subscriptions: true
+				},
+				comments: {
+					message: true,
+					id: true,
+					user: {
+						id: true,
+						name: true,
+						avatarPath: true,
+						isVerified: true,
+						subscribersCount: true
+					}
+				}
+			}
+		})
+
+		if (!video) throw new NotFoundException('Видео не найдено.')
+		return video
+	}
+
+	async update(id: number, dto: VideoDto) {
+		const video = await this.getVideoById(id)
+
+		return this.videoRepository.save({
+			...video,
+			...dto
+		})
+	}
+
+	async getAllVideos(searchTerm?: string) {
+		let options: FindOptionsWhereProperty<VideoEntity> = {}
+
+		if (searchTerm) {
+			options = {
+				name: ILike(`%${searchTerm}`)
+			}
+		}
+
+		return this.videoRepository.find({
+			where: {
+				...options,
+				isPublic: true
+			},
+			order: {
+				createdAt: 'DESC'
+			},
+			relations: {
+				user: true,
+				comments: {
+					user: true
+				}
+			},
+			select: {
+				user: {
+					id: true,
+					name: true,
+					avatarPath: true,
+					isVerified: true
+				}
+			}
+		})
+	}
+
+	async getMostPopularByViews() {
+		return this.videoRepository.find({
+			where: {
+				views: MoreThan(0)
+			},
+			relations: {
+				user: true
+			},
+			select: {
+				user: {
+					id: true,
+					name: true,
+					avatarPath: true,
+					isVerified: true
+				}
+			},
+			order: {
+				views: -1
+			}
+		})
+	}
+
+	async createVideo(userId: number) {
+		const defaultValue = {
+			name: '',
+			user: { id: userId },
+			videoPath: '',
+			description: '',
+			thumbnailPath: ''
+		}
+
+		const newVideo = this.videoRepository.create(defaultValue)
+		const video = await this.videoRepository.save(newVideo)
+		return video
+	}
+
+	async deleteVideo(id: number) {
+		return this.videoRepository.delete({ id })
+	}
+
+	async updateCountViews(id: number) {
+		const video = await this.getVideoById(id)
+		video.views++
+		return this.videoRepository.save(video)
+	}
+
+	async updateReaction(id: number) {
+		const video = await this.getVideoById(id)
+		video.likes++
+		return this.videoRepository.save(video)
+	}
+}
